815c0fe6bf532c9d1dbff1b3247baaa5075198f5
unknown <indianic@ind665.inic.local>
2018-07-19 13:12:38 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Fly
+
+##Run "npm run babel"
+
+##run "live-server" or "npm run start"
+
+## Task runner is not implemented. Gulp need to be implemented.
+
+## Code should be based on Demo JSON. 
+## Actual JSON differ from this so Code will varry there.
+
---
+++ js/main.js
@@ -0,0 +1,103 @@
+"use strict";
+
+var flightJson = [];
+
+$.ajax({ url: "./fly.json",
+        context: document.body,
+        success: (data) => {
+          flightJson = data;
+        },
+        error: () => {
+          alert("API FAILED!!!");
+        }});
+ 
+document.getElementById("result").classList.add("Hide");
+
+
+  var filterData ={};
+  var search = (filterData) => {
+
+
+    let arrData = "";
+    let departDate = filterData.departDate;
+    let seatType = filterData.class ? filterData.class : "eco";
+    let price = filterData.price ? filterData.price : 0;
+    let airline = filterData.airline ? filterData.airline : "";
+    flightJson.forEach((ele) => {
+      let sd = new Date(ele.departDate);
+      if(ele.to == filterData.to  && ele.from == filterData.from) {
+          if(+sd === +departDate) {
+            if ((ele.class == seatType && ele.price >= price)) {
+              if(ele.airline === airline) {
+                  arrData += ` 
+                                    <p>
+                                        <span class="show-result-data">${ele.from}</span>
+                                        <span class="show-result-data"> ${ele.to}</span>
+                                        <span class="show-result-data">${ele.price}</span>
+                                        <span class="show-result-data">${ele.airline}</span>
+                                    </p>                          
+                  `
+                  
+              } else if(!airline) {
+                  arrData += ` 
+                                    <p>
+                                        <span class="show-result-data">${ele.from}</span>
+                                        <span class="show-result-data"> ${ele.to}</span>
+                                        <span class="show-result-data">${ele.price}</span>
+                                        <span class="show-result-data">${ele.airline}</span>
+                                    </p>                          
+                  `
+              }
+            } 
+          }
+        }
+    });
+    document.getElementById("result").classList.add("Show");
+    if(arrData){
+      document.getElementById("resultData").innerHTML = arrData;
+    }else{
+      document.getElementById("resultData").innerHTML = "No Flight Found";
+    }
+  }
+  ///Specific Search Criteria 
+  document.getElementById('specificSearchBtn').addEventListener('click', function () {
+    filterData.from = $("#selectForm").val() ?  $("#selectForm").val() : "";
+    filterData.to =$("#toForm").val() ? $("#toForm").val() : "";
+    filterData.departDate = $("#departDate").val() ? new Date($("#departDate").val()) : "";
+    filterData.airline = $("#airline").val() ? $("#airline").val() : "";
+    filterData.price = $("#price").val() ? $("#price").val() : 0;
+    filterData.class = $("#classBuss").val() ? $("#classBuss").val() : "eco";
+    
+    search(filterData);
+  });
+ 
+  
+  //Listen the click event on Search Button
+  document.getElementById('btnSearch').addEventListener('click', function () {
+    var filterData = {};
+    filterData.from = $("#selectForm").val() ?  $("#selectForm").val() : "";
+    filterData.to = $("#toForm").val() ? $("#toForm").val() : "";
+    filterData.departDate = $("#departDate").val() ? new Date($("#departDate").val()) : "";
+    filterData.price = $("#price").val() ? $("#price").val() : 0;
+    filterData.class = $("#classBusiness").val() ? $("#classBusiness").val() : "eco";
+    filterData.passenger = $("#passenger").val() ? $("#passenger").val() : "";
+    filterData.airline = "";
+    
+    search(filterData);
+  });
+
+  /* 
+  // Listen Radio button based on Oneway Twoway.
+  */
+  var flightWay = (e) => {
+    if (document.querySelector('input[name="flightReturn"]:checked').value) {
+      let radioVal = document.querySelector('input[name="flightReturn"]:checked').value;
+          if (radioVal == "one") {
+            $("#returnDateDiv").hide();
+          }else{
+            $("#returnDateDiv").show();
+          }
+          return radioVal || "one";
+    }
+  }
+
